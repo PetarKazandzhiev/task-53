246bb4a7565b1a40b62aa98958f8ead8d3b383f7
Petar Kazandzhiev <petar.kazandzhiev@gmail.com>
2023-03-03 15:43:53 +0200

    did it

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,10 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
 
+  let product = document.getElementsByClassName('.product');
+
+  let price =  document.getElementsByClassName('.price')
+
+  product.setAttribute('data-price', price.value)
+
 });
